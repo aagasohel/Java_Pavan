35d1ee2b885dbc26d9be8f691e252093e2d511ec
aagasohel <164184132+aagasohel@users.noreply.github.com>
2025-11-28 15:37:04 +0530

    Update FirstProg.java

--- java_Pavan/src/IntroProg/FirstProg.java
+++ java_Pavan/src/IntroProg/FirstProg.java
@@ -5,6 +5,8 @@ public class FirstProg {
 	public static void main(String[] args) {
 		System.out.println("welcome to java");
 		System.out.println("Hello World...");
+		System.out.println("Hello World...");
+		System.out.println("Hello World...");
 	}
 
 }
