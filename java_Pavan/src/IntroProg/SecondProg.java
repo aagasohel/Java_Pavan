ff2527841166a35c04dbefe25c7d6aaeb263d8a8
aagasohel <aagashoel12@gmail.com>
2025-11-28 16:00:18 +0530

    Updated second prog class

--- java_Pavan/src/IntroProg/SecondProg.java
+++ java_Pavan/src/IntroProg/SecondProg.java
@@ -6,6 +6,7 @@ public class SecondProg {
 		System.out.println("welcome"); //welcome
 		System.out.println(1 + 2);  //3
 
+		System.out.println("Sample");
 		
 		System.out.println("1+2"); // 12
 		System.out.println("welcome"+123);// welcome123
